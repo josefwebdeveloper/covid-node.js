4149f3912f6adc0c141654f0172a1efec2044e87
Josef Shainskiy <josef2011@gmail.com>
2021-03-26 14:12:46 +0300

    first commit

---
+++ _helpers/db.js
@@ -0,0 +1,13 @@
+const config = require('config.json');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, {
+  useCreateIndex: true, useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+
+module.exports = {
+  User: require('../users/user.model'),
+  Article: require('../article/article.model'),
+  Timer: require('../timer/timer.model'),
+};
