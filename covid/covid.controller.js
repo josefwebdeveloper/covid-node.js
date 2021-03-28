ea73c63c2d98d1c5384a553069f0b14a4e80c130
Josef Shainskiy <josef2011@gmail.com>
2021-03-28 22:13:34 +0300

    add api

--- covid/covid.controller.js
+++ covid/covid.controller.js
@@ -6,6 +6,7 @@ const axios = require('axios');
 // router.post('/create', create);
 router.get('/us/current', getUsCurrent);
 router.get('/states/current', getStatesCurrent);
+router.get('/states/daily', getStatesDaily);
 // router.get('/getNext/:id', getNext);
 
 // router.put('/:articleId', updateById);
@@ -28,16 +29,41 @@ module.exports = router;
 function getUsCurrent(req, res, next) {
     axios.get('http://covidtracking.com/api/us')
         .then(response => {
-            return response.data ? res.json({data: {response: response.data,statusText:response.statusText}}) : res.sendStatus(404)
+            return response.data ? res.json({
+                data: {
+                    response: response.data,
+                    statusText: response.statusText
+                }
+            }) : res.sendStatus(404)
         })
         .catch(error => {
             return next(error)
         });
 }
+function getStatesDaily(req, res, next) {
+    axios.get('http://covidtracking.com/api/states/daily')
+        .then(response => {
+            return response.data ? res.json({
+                data: {
+                    response: response.data,
+                    statusText: response.statusText
+                }
+            }) : res.sendStatus(404)
+        })
+        .catch(error => {
+            return next(error)
+        });
+}
+
 function getStatesCurrent(req, res, next) {
     axios.get('https://covidtracking.com/api/states')
         .then(response => {
-            return response.data ? res.json({data: {response: response.data,statusText:response.statusText}}) : res.sendStatus(404)
+            return response.data ? res.json({
+                data: {
+                    response: response.data,
+                    statusText: response.statusText
+                }
+            }) : res.sendStatus(404)
         })
         .catch(error => {
             return next(error)
